f19d96179d80a2dfadfc431a7b0890f59d1c1269
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-09 14:22:23 +0800

    [mabel] Edit express_basic_example_with_router to print out the server address

--- express_basic_example_with_router.js
+++ express_basic_example_with_router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
+
 const bookRouter = require("./routes/books");
 const userRouter = require("./routes/users");
 
 app.use("/books", bookRouter);
 app.use("/users", userRouter);
 
-const server = app.listen(3000, function() {
-  console.log("Application started....");
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
