c0bf4df5838c0330fe2068b68503be7bd8ba09c6
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-10 14:36:55 +0800

    [mabel] Edit route parameter example 2with more output

--- route_parameter_example_2.js
+++ route_parameter_example_2.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.param("userId", function(req, res, next, userId) {
-    // do whatever you like with the userId, e.g. looking up the user profile in database
-    const userName = "Tom";
-    const user = {userId, userName}
+app.param("userId", (req, res, next, userId) => {
+  // do whatever you like with the userId, e.g. looking up the user profile in database
+  const userName = "Tom";
+  const user = { userId, userName };
 
-    // and you can set new information in the request object
-    req.user = user;
+  console.log(`req.user is originally: ${req.user}`);
+  // and you can set new information in the request object
+  req.user = user;
+  console.log(`req.user is now: ${req.user}`);
 
-    // now let the next handler to process the request
-    next();
+  // now let the next handler to process the request
+  next();
 });
 
-app.get("/users/:userId/books/:bookId", function(req, res) {
+app.get("/users/:userId", (req, res) => {
+  // now you have access to the req.user that was created in the previous handler
   const user = req.user;
-  res.send("Got the request for user " + user.userName);
+  res.send(
+    `Got the request for user ${user.userName} whose id is ${user.userId}`
+  );
 });
 
-const server = app.listen(3000, function() {
-  console.log("Application started....");
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
