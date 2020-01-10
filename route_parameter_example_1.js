db54da5cff8ed92c386f30a7900c04ffb137d076
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-10 14:32:21 +0800

    [mabel] Edit route parameter example to use arrow function

--- route_parameter_example_1.js
+++ route_parameter_example_1.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.get("/users/:userId/books/:bookId", function(req, res) {
+app.get("/users/:userId/books/:bookId", (req, res) => {
   res.send(req.params);
 });
 
-const server = app.listen(3000, function() {
-  console.log("Application started....");
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
