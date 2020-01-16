44e3f89234d9e57076c855a7939cc2f5370500e2
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-17 01:44:41 +0800

    [mabel] Add testing_example_1 with jest and supertest

---
+++ testing_example_1.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my homepage");
+});
+
+app.get("/books", (req, res) => {
+  res.send("You requested a list of books....");
+});
+
+app.get("/books/:bookId", (req, res) => {
+  res.send(`You request information on book ${req.params.bookId}`);
+});
+
+const requireJsonContent = (req, res, next) => {
+  if (req.headers["content-type"] !== "application/json") {
+    res.status(400).send("Server wants application/json!");
+  } else {
+    next();
+  }
+};
+
+app.post("/", requireJsonContent, (req, res, next) => {
+  res.send("Thanks for the JSON!");
+});
+
+module.exports = app;
---
+++ testing_example_1_index.js
@@ -0,0 +1,5 @@
+const app = require("./testing_example_1");
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
