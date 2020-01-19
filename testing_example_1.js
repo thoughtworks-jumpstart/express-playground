f683f020028da6ba21cad3e66469ecdc538e8ebc
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-01-20 03:06:57 +0800

    [mabel] Minor edits to testing example 1 and test

--- testing_example_1.js
+++ testing_example_1.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to my homepage");
--- testing_example_1.test.js
+++ testing_example_1.test.js
@@ -1,59 +1,61 @@
 const request = require("supertest");
 const app = require("./testing_example_1");
 
-describe("GET /", () => {
-  it("should respond correctly", async done => {
-    const response = await request(app)
-      .get("/")
-      .expect(200);
-    expect(response.text).toEqual("Welcome to my homepage");
-    done();
+describe("testing example 1", () => {
+  describe("GET /", () => {
+    it("should respond correctly with success code", async done => {
+      const response = await request(app)
+        .get("/")
+        .expect(200);
+      expect(response.text).toEqual("Welcome to my homepage");
+      done();
+    });
+
+    it("should respond correctly with success code", async done => {
+      const { text } = await request(app)
+        .get("/")
+        .expect(200);
+      expect(text).toEqual("Welcome to my homepage");
+      done();
+    });
   });
 
-  it("should respond correctly", async done => {
-    const { text } = await request(app)
-      .get("/")
-      .expect(200);
-    expect(text).toEqual("Welcome to my homepage");
-    done();
-  });
-});
+  describe("GET /books", () => {
+    it("should respond correctly with success code", async done => {
+      const response = await request(app)
+        .get("/books")
+        .expect(200);
+      expect(response.text).toEqual("You requested a list of books....");
+      done();
+    });
 
-describe("GET /books", () => {
-  it("should respond correctly", async done => {
-    const response = await request(app)
-      .get("/books")
-      .expect(200);
-    expect(response.text).toEqual("You requested a list of books....");
-    done();
-  });
-
-  it("should get a book by id", async done => {
-    const id = "3";
-    const response = await request(app)
-      .get(`/books/${id}`)
-      .expect(200);
-    expect(response.text).toEqual(`You request information on book ${id}`);
-    done();
-  });
-});
-
-describe("POST /", () => {
-  it("should respond correctly when sending json", async done => {
-    const { text } = await request(app)
-      .post("/")
-      .send({ thisIsJson: "json!" })
-      .expect(200);
-    expect(text).toEqual("Thanks for the JSON!");
-    done();
+    it("should get a book by id", async done => {
+      const id = "3";
+      const response = await request(app)
+        .get(`/books/${id}`)
+        .expect(200);
+      expect(response.text).toEqual(`You request information on book ${id}`);
+      done();
+    });
   });
 
-  it("should respond with status 400 and correct string when sending non-json", async done => {
-    const { text } = await request(app)
-      .post("/")
-      .send("This is not json!")
-      .expect(400);
-    expect(text).toEqual("Server wants application/json!");
-    done();
+  describe("POST /", () => {
+    it("should respond correctly when sending json", async done => {
+      const { text } = await request(app)
+        .post("/")
+        .send({ thisIsJson: "json!" })
+        .expect(200);
+      expect(text).toEqual("Thanks for the JSON!");
+      done();
+    });
+
+    it("should respond with status 400 and correct string when sending non-json", async done => {
+      const { text } = await request(app)
+        .post("/")
+        .send("This is not json!")
+        .expect(400);
+      expect(text).toEqual("Server wants application/json!");
+      done();
+    });
   });
 });
